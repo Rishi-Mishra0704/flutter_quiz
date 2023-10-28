ccc128fdbe44f7df4d394ff67504393d50a260db
Rishi-Mishra0704 <rishimishra0404@gmail.com>
2023-10-28 22:44:25 +0530

    update answer button style

--- lib/answer_button.dart
+++ lib/answer_button.dart
@@ -1,28 +1,32 @@
 import 'package:flutter/material.dart';
 
 class AnswerButton extends StatelessWidget {
-  const AnswerButton(
-      {required this.answerText, required this.onTap, super.key});
+  const AnswerButton({required this.answerText, required this.onTap, Key? key})
+      : super(key: key);
 
   final String answerText;
   final void Function() onTap;
 
   @override
   Widget build(BuildContext context) {
-    return ElevatedButton(
-      onPressed: onTap,
-      style: ElevatedButton.styleFrom(
-        padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 20),
-        backgroundColor: Colors.blue,
-        foregroundColor: Colors.white,
-        shape: const RoundedRectangleBorder(
-          borderRadius: BorderRadius.all(Radius.circular(40)),
+    return Padding(
+      padding: const EdgeInsets.symmetric(
+          vertical: 10, horizontal: 16), // Adjust margin as needed
+      child: ElevatedButton(
+        onPressed: onTap,
+        style: ElevatedButton.styleFrom(
+          padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 20),
+          backgroundColor: Colors.blue,
+          foregroundColor: Colors.white,
+          shape: const RoundedRectangleBorder(
+            borderRadius: BorderRadius.all(Radius.circular(40)),
+          ),
         ),
-      ),
-      child: Text(
-        answerText,
-        style: const TextStyle(fontSize: 20),
-        textAlign: TextAlign.center,
+        child: Text(
+          answerText,
+          style: const TextStyle(fontSize: 20),
+          textAlign: TextAlign.center,
+        ),
       ),
     );
   }
