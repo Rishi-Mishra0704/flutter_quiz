b3c7c18c066323ab23fbd8e7d611f01d2b9c3297
Rishi-Mishra0704 <rishimishra0404@gmail.com>
2023-10-28 18:52:03 +0530

    refactor screens

--- lib/main.dart
+++ lib/main.dart
@@ -1,24 +1,9 @@
 import 'package:flutter/material.dart';
-import 'package:quiz_app/start_screen.dart';
+import 'package:quiz_app/quiz.dart';
+
 
 void main() {
   runApp(
-    MaterialApp(
-      home: Scaffold(
-        body: Container(
-          decoration: const BoxDecoration(
-            gradient: LinearGradient(
-              colors: [
-                Color.fromARGB(255, 8, 40, 253),
-                Color.fromARGB(255, 15, 148, 168),
-              ],
-              begin: Alignment.topLeft,
-              end: Alignment.bottomRight,
-            ),
-          ),
-          child: const StartScreen(),
-        ),
-      ),
-    ),
+   const Quiz()
   );
 }
